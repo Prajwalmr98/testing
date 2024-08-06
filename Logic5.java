2515cca8ce2c451906054ad999b0a63c96f7859e
unknown <prajwalmr98@gmail.com>
2024-08-06 22:15:42 +0530

    s

---
+++ Logic5.java
@@ -0,0 +1,16 @@
+class Logic5
+{
+ public static void main(String[] args)
+ {
+  int x=20;
+  int y=30;
+  if(x<=y)
+  {
+   System.out.println("Hii");
+  }
+  else
+  {
+  System.out.println("Hello");
+  }
+ }
+}
